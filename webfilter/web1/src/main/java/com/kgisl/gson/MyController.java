1a07ee56149ef60525ced21adeac2a81e06f42ac
madhanm051099 <114131708+madhanm051099@users.noreply.github.com>
2022-09-23 10:41:40 +0530

    Add files via upload

---
+++ webfilter/web1/src/main/java/com/kgisl/gson/MyController.java
@@ -0,0 +1,18 @@
+package com.kgisl.gson;
+//import java.text.DateFormat;
+//import java.util.Date;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
+@RequestMapping("/home")
+public class MyController {
+    @RequestMapping(method = RequestMethod.GET)
+	public String home(Model model) {
+		System.out.println("MyController Requested, locale = ");
+		model.addAttribute("serverTime", "formattedDate");
+		return "home";
+	}  
+}
